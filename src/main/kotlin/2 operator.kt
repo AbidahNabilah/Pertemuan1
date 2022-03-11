ad621b75aa2991113ca165a37f45e0dcbada6e86
Abidah Nabilah <abdhnabilah@gmail.com>
2022-03-11 15:53:12 +0700

    Percobaan operator

---
+++ src/main/kotlin/2 operator.kt
@@ -0,0 +1,19 @@
+// penggunaan operator increment
+fun main() {
+    // untuk komentar 1 baris
+    /*
+    komentar untuk lebih dari 1 baris
+    */
+    /*
+    * untuk mendokumetasi kode program
+    */
+    var a = 7
+    var b = 6
+    var c = (a++ + b++)/2.0
+    println(c + b)
+    // penggunaan 2 jenis variabel statis dan dinamis
+    var dimanis = "hadir"
+    var statis = 2
+    println(dimanis)
+    println(statis)
+}
