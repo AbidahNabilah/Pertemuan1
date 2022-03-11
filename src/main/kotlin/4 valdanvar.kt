dfda865aec0fd1593a845c03429de7e2c8ab932a
Abidah Nabilah <abdhnabilah@gmail.com>
2022-03-11 16:24:26 +0700

    Percobaan var dan val

---
+++ src/main/kotlin/4 valdanvar.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    // penggunaan kode program yang menggunakan var variabel
+    var varVariabel: Int
+    varVariabel = 56
+    println("Pemberian nilai pertama pada varVariabel = " +varVariabel)
+    varVariabel = 78
+    println("Pemberian nilai kedua pada varVariabel =" +varVariabel)
+
+    // penggunaan kode program yang menggunakan val variabel
+    val phi = 3.14
+
+
+
+}
