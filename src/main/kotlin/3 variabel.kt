197f7559f72e3318d62e1444780828f9427714e1
Abidah Nabilah <abdhnabilah@gmail.com>
2022-03-11 16:17:41 +0700

    Percobaan varaibel

---
+++ src/main/kotlin/3 variabel.kt
@@ -0,0 +1,10 @@
+// penggunaan variabel
+fun main() {
+    // deklarasi dan inisialisasi dari variabel tipe dinamic
+    var dynamicVar = 2
+    println("contoh dynamic variabel = " + dynamicVar)
+
+    // deklarasi dan inisialisasi dari variabel tipe static
+    var staticVar: Char = 'A'
+    println("contoh static variabel = " + staticVar)
+}
