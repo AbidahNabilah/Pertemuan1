86dbc11c7556cbf3578f6e8821b774de0a7d9df6
Abidah Nabilah <abdhnabilah@gmail.com>
2022-03-12 11:56:14 +0700

    Percobaan Underscore Literal

---
+++ src/main/kotlin/6 underscoreliteral.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    // deklarasi menggunakan literal float
+    val floatLiteral : Float = 178.95F
+    println("Contoh Literal Float : " + floatLiteral)
+
+    //deklarasi menggunakan literal long
+    val oneBillion: Long = 1000000000L
+    println("Contoh Literal Long : " +oneBillion)
+
+    // deklarasi menggunakan heksadesimal diawali dengan 0x
+    val valHeka = 0x0F
+    println("Contoh Literal Heksadesimal : " + valHeka)
+
+    // deklarasi menggunakan literal binary dengan 0b
+    val valBinary = 0b00001011
+    println("Contoh Literal Binary : " + valBinary)
+
+    // deklrasi menggunakan notasi konvesional
+    val noatsiKonvesional = 123.5e10
+    println("Contoh Literal Konvesioanal Floating Point Number : " + noatsiKonvesional)
+
+}
